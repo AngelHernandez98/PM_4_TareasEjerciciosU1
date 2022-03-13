80298cc084d3f7a2cf02f858ec9f22d181177c38
AngelHernandez98 <89492175+AngelHernandez98@users.noreply.github.com>
2022-03-13 11:40:53 -0600

    Add files via upload

---
+++ PM_4_TareasEjerciciosU1/N0_HojaPresentacion.asm
@@ -0,0 +1,40 @@
+TITLE Introduccion
+
+; DESCRPICIÓN
+; Objetivo: Archivo de Ejemplo
+
+; Autore(s):
+; Mtro. Alejandro Garcia
+; Su Nombre.!
+
+; Semestre: 8vo Semestre ISC
+; FIN DESCRIPCIÓN
+
+INCLUDE Irvine32.inc
+
+.data
+
+; Área de Declaración de Variables
+var0_2 db "            Universidad Autonoma de Tamaulipas"
+var1_2 db "                                 Campus Tampico"
+var2_2 db "                              Facultad de Ingenieria Arturo Narro Siller"
+var3_2 db "                          Integrantes del equipo:"
+var4_2 db "                           *Hernadez Hernandez Angel Anastasio"
+var5_2 db "                         *Torres Orozco David Jonatan"
+var6_2 db "                            *Hernandez Licea Luis Armando"
+var7_2 db "                   Profesor: Dr. Garcia Ruiz Alejandro Humberto"
+var8_2 db "             Asigantura: Programacion de Microprocesadores"
+var9_2 db "                       8vo. Semestre - Grupo I"
+var10_2 db "                                           2022-1"
+.code 
+
+main0 PROC
+;Lógica del Programa
+
+;call waitmsg
+
+exit
+
+main0 ENDP
+
+END main0
